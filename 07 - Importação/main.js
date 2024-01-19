360990192681b1ea6e29cbfc060d5956f1174f72
Júlio Gabriel <113954423+JulioGSilva@users.noreply.github.com>
2024-01-18 22:47:50 -0300

    att

--- 07 - Importação/main.js
+++ 07 - Importação/main.js
@@ -1,21 +1,12 @@
 const {gets, print} = require('./funcoes-auxiliares')
 
-// const lista = [8, 9, 36, 2, 55, 16, 95];
-// let res = 0;
+const lista = [8, 9, 36, 2, 55, 16, 95];
+let res = 0;
 
-// for(let i = 1; i < lista.length; i++) {
-//     if (lista[i] > res) {
-//         res = lista[i]
-//     }
-// }
-
-// console.log(`O maior número da lista é ${res}`);
-
-const lista = [];
-
-for (let i = 0; i < i < 5; i++) {
-    const numSorteado = gets();
-    lista.push(numSorteado);
+for(let i = 1; i < lista.length; i++) {
+    if (lista[i] > res) {
+        res = lista[i]
+    }
 }
 
-print(numSorteado)
+console.log(`O maior número da lista é ${res}`);
